c87f32cd5c39262ed15a0a3e26d549052bf32eb9
Emil <Starostin.emil@mail.ru>
2023-01-04 15:27:46 +0300

    Initial commit

---
+++ second/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace second.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
