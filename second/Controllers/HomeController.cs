83afcb90cc3d6ffa339618806e423daca1a8b27a
Emil <Starostin.emil@mail.ru>
2023-01-06 20:30:11 +0300

    4

--- second/Controllers/HomeController.cs
+++ second/Controllers/HomeController.cs
@@ -9,16 +9,13 @@ namespace second.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IRobotRepository _robotRepository;
-        public HomeController(IRobotRepository rr)
-        {
-            _robotRepository = rr;
-        }
+      
+        
 
 
         public  IActionResult Index()
         {
-            var resp = _robotRepository.Select();
+           
 
             return View();
         }
---
+++ second/Controllers/RobotController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using second.DAL.Interfaces;
+
+namespace second.Controllers
+{
+    public class RobotController : Controller
+    {
+        private readonly IRobotRepository _robotRepository;
+        public RobotController(IRobotRepository rr)
+        {
+            _robotRepository = rr;
+        }
+        [HttpGet]
+        public IActionResult GetRobots()
+        {
+            var resp = _robotRepository.Select();
+            return View(resp);
+        }
+    }
+}
